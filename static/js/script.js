4fba76e7b75f4aeb20a80bdd6505a5bd728f61fd
amirreza <amirreza.firoz13@gmail.com>
2023-10-22 22:56:48 +0330

    improved view and some responsive

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,20 @@ document.querySelector('#equal').addEventListener('click', c => {
         })
     })
 });
+
+const step = 'none';
+var view = document.querySelector('.view #box');
+
+document.querySelectorAll('#t2 li div').forEach(i => {
+    i.addEventListener('click', c => {
+        let content = c.target.innerHTML;
+
+        view.innerHTML = view.innerHTML == '0' ? '' : view.innerHTML;
+
+        switch (step) {
+            case 'none':
+                view.innerHTML += content;
+                break;
+        }
+    })
+});
